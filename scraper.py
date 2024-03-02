76f88f0f26b3b49a0f538d24ee98fe3e83e9369a
Harmanpreet Singh <singhharmanpreet89935@gmail.com>
2024-03-02 21:49:39 +0530

    Improve data_cleaning function

--- scraper.py
+++ scraper.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 from selenium.webdriver.edge.options import Options
 import pandas as pd
 import time
+
+
+def duration_in_minutes(duration):
+    if type(duration) is not str:
+        return duration
+    total_time = 0
+    durations = duration.split('and')
+    for duration in durations:
+        time_quantity, abbreviation = duration.split()
+        time_quantity = int(time_quantity)
+        if abbreviation in ['hrs', 'hr']:
+            total_time += time_quantity*60
+        elif abbreviation in ['mins', 'min']:
+            total_time += time_quantity
+    return total_time
+
 
 def data_cleaning(data):
     # Function to clean up data columns
@@ -10,12 +27,20 @@ def data_cleaning(data):
     data.narrator = data.narrator.apply(
         lambda x: x[len('Narrated by:'):].strip() if x.startswith('Narrated by:') else x)
     data.series = data.series.apply(lambda x: x[len('Series:'):].strip() if x.startswith('Series:') else x)
+
     data.runtime = data.runtime.apply(lambda x: x[len('Length:'):].strip() if x.startswith('Length:') else x)
+    data.runtime = data.runtime.apply(duration_in_minutes)
+
     data.release_date = data.release_date.apply(
         lambda x: x[len('Release Date:'):].strip() if x.startswith('Release Date:') else x)
-    data.total_ratings = data.total_ratings.apply(lambda x: x.split(' ')[0] if 'rating' in x else x)
-    data.stars = data.stars.apply(lambda x: x.split(' ')[0] if 'stars' in x else x)
+    data.language = data.language.apply(lambda x: x.split(' ')[-1])
+    data.total_ratings = data.total_ratings.apply(lambda x: x.split(' ')[0] if 'rating' in x else '0' if x == 'NA' else x)
+    data.stars = data.stars.apply(lambda x: x.split(' ')[0] if 'stars' in x else '0' if x == 'Not rated yet' else x)
+
+    data.price = data.price.str.replace('Listen for free', '0')
+    data.price = data.price.str.replace('₹', '').str.replace(',', '')
     return data
+
 
 options = Options()
 # options.add_argument('--headless') # to turn on headless mode
@@ -68,6 +93,8 @@ for i in range(total_pages):
                         audiobook_dict[key] = book.find_element(by='xpath', value=value[0]).text
                     except:
                         audiobook_dict[key] = book.find_element(by='xpath', value=value[1]).text
+            except exceptions.NoSuchElementException:
+                print('"{}" is not available for book name "{}"'.format(key, audiobook_dict['title']))
             except Exception as e:
                 print(e)
         # Concatenate extracted data into DataFrame
@@ -85,4 +112,6 @@ driver.quit()
 
 # Clean up data and save to CSV
 data = data_cleaning(data)
+data.rename(columns={'runtime': 'runtime (in minutes)', 'stars': 'stars (out of 5)', 'price': 'price (in rupees)'},
+            inplace=True)
 data.to_csv('audiobooks.csv', index=False)
